690e3a4deea41246efd93c3289f228c3602da63f
Trần Trọng Nhân-21095871 <nhanmpt000@gmail.com>
2023-04-29 03:03:11 +0700

    ChiTietSanPham_3SanPhamDauCuaNam

---
+++ assets/js/dataDanhSachSanPham..js
@@ -0,0 +1,28 @@
+let sp1 = {
+    masp:1,
+    tensp:"Giày Thể Thao Nam Biti's Hunter Core HSM000500REU (Rêu)",
+    hinhanh:'/assets/img/GiayNam/hsm000500reu__10__b1318ea901524a578aeaec64bbd025df_grande.jpg',
+    dongia:"785,000 ₫",
+    mota :' Phần thân trên/ upper dệt/ Knits thoải mái, co giản. <br>- Đế: Phylon/ Cao su tạo độ uốn gấp tốt. <br> <br> - Có dây buộc. <br> <br> - Do màn hình và điều kiện ánh sáng khác nhau, màu sác thực tế của sản phẩm có thể chênh lệch khảng 3-5%.'
+}
+
+let sp2 = {
+    masp:2,
+    tensp:"Giày Thể Thao Nam Biti's Hunter Core HSM000500DEN (Đen)",
+    hinhanh:'/assets/img/GiayNam/hsm000500den__10__76b2e099477f4b91b9cb90997b8284ad_grande.jpg',
+    dongia:"785,000 ₫",
+    mota :"Giày Thể Thao Nam Biti's Hunter Core  được yêu thích bởi thiết kế đầy mạnh mẽ, trẻ trung và cá tính. Với bộ đế độc quyền từ Biti’s Hunter không chỉ giúp đôi chân cảm giác “Nhẹ như bay” mà còn tăng vượt bậc độ đàn hồi, tạo độ linh hoạt và tự tin hơn khi chiều cao đế đạt tới 5cm."
+}
+
+let sp3 = {
+    masp:3,
+    tensp:"Giày Thể Thao Nam Hunter Core - Gravity Leap collection DSMH10900XAM (Xám)",
+    hinhanh:'/assets/img/GiayNam/dsmh10900xam__10__d23e9382a6684710b18d1306378279a5_grande.jpg',
+    dongia:"773,100 ₫",
+    mota :"Giày Thể Thao Nam Hunter Core - Gravity Leap collection  được yêu thích bởi thiết kế đầy mạnh mẽ, trẻ trung và cá tính. Với bộ đế độc quyền từ Biti’s Hunter không chỉ giúp đôi chân cảm giác “Nhẹ như bay” mà còn tăng vượt bậc độ đàn hồi, tạo độ linh hoạt và tự tin hơn khi chiều cao đế đạt tới 5cm."
+}
+
+
+let dssp = [sp1,sp2,sp3]
+
+localStorage.setItem("dssp",JSON.stringify(dssp))
